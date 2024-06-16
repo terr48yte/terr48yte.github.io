950a8e59217d8c5a7cea825fc47050951bf19f36
shironxn <qshiro69@gmail.com>
2024-06-16 23:25:29 +0700

    feat: initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
+/** @type {import('tailwindcss').Config} */
+export default withMT({
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});

cb3bbd3b5dcefa25070b30b49728731880724165
kawenzy <136422933+kawenzy@users.noreply.github.com>
2024-07-04 21:46:22 +0700

    update meta

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -14,6 +14,10 @@ import {
 } from "lucide-react";
 import { useState, type ReactNode } from "react";
 import { useCopyToClipboard } from "usehooks-ts";
+import Discord  from "@assets/discord.png"
+import Insta from "@assets/instagram.webp"
+import Git from "@assets/github.png"
+import Yt from "@assets/youtube.jpg"
 
 type Props = {
   name: string;
@@ -27,7 +31,7 @@ const props: Props = [
   {
     name: "Instagram",
     icon: <Instagram size={40} color="#FCFEFF" />,
-    img: "https://wallpapercave.com/wp/wp5979901.jpg",
+    img: Insta.src,
     url: "https://instagram.com/itclub48",
     desc: "@itclub48 | 🍜"
   },
@@ -38,7 +42,7 @@ const props: Props = [
         <path d="M524.5 69.8a1.5 1.5 0 0 0 -.8-.7A485.1 485.1 0 0 0 404.1 32a1.8 1.8 0 0 0 -1.9 .9 337.5 337.5 0 0 0 -14.9 30.6 447.8 447.8 0 0 0 -134.4 0 309.5 309.5 0 0 0 -15.1-30.6 1.9 1.9 0 0 0 -1.9-.9A483.7 483.7 0 0 0 116.1 69.1a1.7 1.7 0 0 0 -.8 .7C39.1 183.7 18.2 294.7 28.4 404.4a2 2 0 0 0 .8 1.4A487.7 487.7 0 0 0 176 479.9a1.9 1.9 0 0 0 2.1-.7A348.2 348.2 0 0 0 208.1 430.4a1.9 1.9 0 0 0 -1-2.6 321.2 321.2 0 0 1 -45.9-21.9 1.9 1.9 0 0 1 -.2-3.1c3.1-2.3 6.2-4.7 9.1-7.1a1.8 1.8 0 0 1 1.9-.3c96.2 43.9 200.4 43.9 295.5 0a1.8 1.8 0 0 1 1.9 .2c2.9 2.4 6 4.9 9.1 7.2a1.9 1.9 0 0 1 -.2 3.1 301.4 301.4 0 0 1 -45.9 21.8 1.9 1.9 0 0 0 -1 2.6 391.1 391.1 0 0 0 30 48.8 1.9 1.9 0 0 0 2.1 .7A486 486 0 0 0 610.7 405.7a1.9 1.9 0 0 0 .8-1.4C623.7 277.6 590.9 167.5 524.5 69.8zM222.5 337.6c-29 0-52.8-26.6-52.8-59.2S193.1 219.1 222.5 219.1c29.7 0 53.3 26.8 52.8 59.2C275.3 311 251.9 337.6 222.5 337.6zm195.4 0c-29 0-52.8-26.6-52.8-59.2S388.4 219.1 417.9 219.1c29.7 0 53.3 26.8 52.8 59.2C470.7 311 447.5 337.6 417.9 337.6z" />
       </svg>
     ),
-    img: "https://graphicsprings.com/wp-content/uploads/2023/08/image-29.png",
+    img: Discord.src,
     url: "https://dsc.gg/terr48yte",
     desc: "@TERR48YTE | the community for technology discussion 😉🍵"
   },
@@ -46,14 +50,14 @@ const props: Props = [
   {
     name: "Github",
     icon: <Github size={40} color="#FCFEFF" />,
-    img: "https://www.marksei.com/wp-content/uploads/2018/06/GitHub-logo.png",
+    img: Git.src,
     url: "https://github.com/terr48yte/",
     desc: "@terr48yte | all project from me 👩‍💻"
   },
   {
     name: "Youtube",
     icon: <Youtube size={40} color="#FCFEFF" />,
-    img: "https://www.vhv.rs/file/max/19/199932_youtube-png.jpg",
+    img: Yt.src,
     url: "https://youtube.com/@terr48yte",
     desc: "@terr48yte | 🍿"
   },
@@ -119,16 +123,11 @@ export default function CardWithLink() {
                   {copied === i ? <Check color="#FCFEFF"/> : <Copy color="#FCFEFF"/>}
                 </IconButton>
               </div>
-              <Typography
-                id="link"
-                placeholder={undefined}
-                onPointerEnterCapture={undefined}
-                onPointerLeaveCapture={undefined}
-                variant="paragraph"
-                className="mt-3 h-12 text-gray-400"
+              <p
+                className="mt-3 h-12 line-clamp-2 text-gray-400 leading-relaxed"
               >
                 {link.desc.toLowerCase()}
-              </Typography>
+              </p>
             </CardBody>
           </Card>
         ))}

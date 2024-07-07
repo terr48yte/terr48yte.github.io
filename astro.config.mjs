979b70da4591f748ca43d80ce9f624d716e81c70
shironxn <qshiro69@gmail.com>
2024-07-07 08:15:43 +0700

    chore: change deploy url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://terr48yte.github.io",
-  base: "/linktree",
   integrations: [tailwind(), react()],
 });

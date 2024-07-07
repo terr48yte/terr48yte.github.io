6fda022612d07301c69b9238f334f5ee4ae2f0d6
shironxn <qshiro69@gmail.com>
2024-07-07 10:57:28 +0700

    chore: change icon and style

---
+++ src/lib/types.ts
@@ -0,0 +1,14 @@
+export interface HeaderProps {
+  img: string;
+  title: string;
+  slogan: string;
+}
+
+export interface CardProps {
+  name: string;
+  icon: string;
+  url: string;
+  img: string;
+  desc: string;
+}
+[];
